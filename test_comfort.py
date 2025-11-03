6ae45b75322fb9a1f1ded95f1c15525906ebb20e
d4nbrn <vmzr0073@leeds.ac.uk>
2025-11-03 10:06:41 +0000

    End of task

--- test_comfort.py
+++ test_comfort.py
@@ -8,9 +8,17 @@
 from weather import comfort_level
 
 def test_cold():
-    assert comfort_level(    ) == "COLD"
-    assert comfort_level(    ) == "COLD"
+    assert comfort_level(15) == "COLD"
+    assert comfort_level(-100) == "COLD"
 
 def test_hot():
-    assert comfort_level(    ) == "HOT"
-    assert comfort_level(    ) == "HOT"
+    assert comfort_level(26) == "HOT"
+    assert comfort_level(100) == "HOT"
+
+def test_OK():
+    assert comfort_level(25) == "OK"
+    assert comfort_level(16) == "OK"
+
+test_cold()
+test_hot()
+test_OK()
